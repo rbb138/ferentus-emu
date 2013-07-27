bad00cce0bced861d4561478ad2f9c3968c3d009
rbb138 <rbb138@hotmail.co.uk>
2013-07-27 00:51:32 +0000

    + Ferentus-Emu empty c# Solution + Login-Server empty c# Project + Debug-Server.exe for experimenting with packets

---
+++ Login-Server/Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace Login_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Creates one SocketPermission object for access restrictions
+                SocketPermission permission = new SocketPermission(
+                NetworkAccess.Accept,     // Allowed to accept connections 
+                TransportType.Tcp,        // Defines transport types 
+                "",                       // The IP addresses of local host 
+                SocketPermission.AllPorts // Specifies all ports 
+                );
+
+                // Listening Socket object 
+                Socket sListener = null;
+
+                // Ensures the code to have permission to access a Socket 
+                permission.Demand();
+
+                // Resolves a host name to an IPHostEntry instance 
+                IPHostEntry ipHost = Dns.GetHostEntry("");
+
+                // Gets first IP address associated with a localhost 
+                IPAddress ipAddr = ipHost.AddressList[0];
+
+                // Creates a network endpoint 
+                IPEndPoint ipEndPoint = new IPEndPoint(ipAddr, 29000);
+
+                // Create one Socket object to listen the incoming connection 
+                sListener = new Socket(
+                    ipAddr.AddressFamily,
+                    SocketType.Stream,
+                    ProtocolType.Tcp
+                    );
+
+                // Associates a Socket with a local endpoint 
+                sListener.Bind(ipEndPoint);
+
+                // Places a Socket in a listening state and specifies the maximum 
+                // Length of the pending connections queue 
+                sListener.Listen(10);
+
+                // Begins an asynchronous operation to accept an attempt 
+                AsyncCallback aCallback = new AsyncCallback(AcceptCallback);
+                sListener.BeginAccept(aCallback, sListener);
+
+                Console.WriteLine("Server is now listening on " + ipEndPoint.Address + " port: " + ipEndPoint.Port);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+                Console.ReadKey();
+            }
+        }
+
+        public void AcceptCallback(IAsyncResult ar)
+        {
+            Socket listener = null;
+
+            // A new Socket to handle remote host communication 
+            Socket handler = null;
+            try
+            {
+                // Receiving byte array 
+                byte[] buffer = new byte[1024];
+                // Get Listening Socket object 
+                listener = (Socket)ar.AsyncState;
+                // Create a new socket 
+                handler = listener.EndAccept(ar);
+
+                // Using the Nagle algorithm 
+                handler.NoDelay = false;
+
+                // Creates one object array for passing data 
+                object[] obj = new object[2];
+                obj[0] = buffer;
+                obj[1] = handler;
+
+                // Begins to asynchronously receive data 
+                handler.BeginReceive(
+                    buffer,        // An array of type Byt for received data 
+                    0,             // The zero-based position in the buffer  
+                    buffer.Length, // The number of bytes to receive 
+                    SocketFlags.None,// Specifies send and receive behaviors 
+                    new AsyncCallback(ReceiveCallback),//An AsyncCallback delegate 
+                    obj            // Specifies infomation for receive operation 
+                    );
+
+                // Begins an asynchronous operation to accept an attempt 
+                AsyncCallback aCallback = new AsyncCallback(AcceptCallback);
+                listener.BeginAccept(aCallback, listener);
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+                Console.ReadKey();
+            }
+        }
+
+        public void ReceiveCallback(IAsyncResult ar)
+        {
+            try
+            {
+                // Fetch a user-defined object that contains information 
+                object[] obj = new object[2];
+                obj = (object[])ar.AsyncState;
+
+                // Received byte array 
+                byte[] buffer = (byte[])obj[0];
+
+                // A Socket to handle remote host communication. 
+                handler = (Socket)obj[1];
+
+                // Received message 
+                string content = string.Empty;
+
+
+                // The number of bytes received. 
+                int bytesRead = handler.EndReceive(ar);
+
+                if (bytesRead > 0)
+                {
+                    content += Encoding.Unicode.GetString(buffer, 0,
+                        bytesRead);
+
+                    // If message contains "<Client Quit>", finish receiving
+                    if (content.IndexOf("<Client Quit>") > -1)
+                    {
+                        // Convert byte array to string
+                        string str = content.Substring(0, content.LastIndexOf("<Client Quit>"));
+
+                        //this is used because the UI couldn't be accessed from an external Thread
+                        this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate()
+                        {
+                            tbAux.Text = "Read " + str.Length * 2 + " bytes from client.\n Data: " + str;
+                        }
+                        );
+                    }
+                    else
+                    {
+                        // Continues to asynchronously receive data
+                        byte[] buffernew = new byte[1024];
+                        obj[0] = buffernew;
+                        obj[1] = handler;
+                        handler.BeginReceive(buffernew, 0, buffernew.Length,
+                            SocketFlags.None,
+                            new AsyncCallback(ReceiveCallback), obj);
+                    }
+
+                    this.Dispatcher.BeginInvoke(DispatcherPriority.Normal, (ThreadStart)delegate()
+                    {
+                        tbAux.Text = content;
+                    }
+                    );
+                }
+            }
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.ToString());
+                Console.ReadKey();
+            }
+        }
+    }
+}
